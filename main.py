06814327348adbe2d3c6a358f7a3ea219d5a6066
Stephen Souvall <stephensouvall@gmail.com>
2023-11-28 21:19:58 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+password_string = ''
+
+for x in range(0, nr_letters):
+    index = random.randint(0, len(letters) - 1)
+    password_string = f'{password_string}{letters[index]}'
+
+for x in range(0, nr_symbols):
+    index = random.randint(0, len(symbols) - 1)
+    password_string = f'{password_string}{symbols[index]}'
+
+for x in range(0, nr_numbers):
+    index = random.randint(0, len(numbers) - 1)
+    password_string = f'{password_string}{numbers[index]}'
+
+shuffled_string = ''.join(random.sample(password_string, len(password_string)))
+
+print(f'Suggested password: {shuffled_string}')
+
